6b51c578e2b409815b4d70c6f27c477bb0ec49db
wasteapp111@gmail.com <Diordi1>
2024-07-01 17:49:39 +0530

    fourth

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+  
+  injectSpeedInsights();
