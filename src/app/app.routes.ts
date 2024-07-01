3d14e6928cfb801a1a3d5328065888b2a79822ac
wasteapp111@gmail.com <Diordi1>
2024-07-01 21:03:07 +0530

    k1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
-    {path:'home',component:HomeComponent},
-    {path:'',redirectTo:"/home",pathMatch:'full'}
-];
+export const routes: Routes = [];
--- src/assets/k1.js
+++
@@ -1,9 +0,0 @@
-function toggle1(){
-    let logo=document.querySelector('.ham-icon1')
-    let menu=document.querySelector('.ham-icon');
-    logo.classList.toggle("open");
-    menu.classList.toggle("open");
-    console.log("was clicked");
-    
-
-}
