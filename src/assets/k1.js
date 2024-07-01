38461a4afdfea7b81bcf5f9e02e02672ac5a5fd9
wasteapp111@gmail.com <Diordi1>
2024-07-01 18:33:39 +0530

    port_update

---
+++ src/assets/k1.js
@@ -0,0 +1,9 @@
+function toggle1(){
+    let logo=document.querySelector('.ham-icon1')
+    let menu=document.querySelector('.ham-icon');
+    logo.classList.toggle("open");
+    menu.classList.toggle("open");
+    console.log("was clicked");
+    
+
+}
